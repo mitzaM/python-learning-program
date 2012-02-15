00c7aa9f09aadb020c0d762d14512349c1a4afc9
Mihai Man <mitaman89@yahoo.com>
2012-02-15 14:14:35 +0200

    added more templates

--- dlp/chestionare/views.py
+++ dlp/chestionare/views.py
@@ -1,17 +1,43 @@
-from dlp.chestionare.models import Test
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from models import Quest, Page
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 def index(request):
-	test_list = Test.objects.all().order_by('name')
-	return render_to_response('index.html',
-				{'test_list': test_list})
+	quest_list = Quest.objects.all().order_by('name')
+	return render_to_response('index.html', {
+				'quest_list': quest_list
+				})
 
-def test(request, test_id):
-	return HttpResponse("This is test %s." % test_id)
+def quest(request, quest_id):
+	q = get_object_or_404(Quest, pk = quest_id)
+	page_list = q.page_set.all()
+	return render_to_response('quest.html', {
+				'questionnaire': q,
+				'page_list': page_list
+				})
 
-def page(request, test_id, page_id):
-	return HttpResponse("This is page %s of test %s." % (page_id, test_id))
+def page(request, quest_id, page_id):
+	p = get_object_or_404(Page, order = page_id, quest = quest_id)
+	q = get_object_or_404(Quest, pk = quest_id)
+	question_list = p.question_set.all()
+	return render_to_response('page.html', {
+				'questionnaire': q,
+				'page': p,
+				'question_list': question_list
+				},
+				context_instance = RequestContext(request))
 
-def results(request, test_id):
-	return HttpResponse("This is the result for %s." % test_id)
+def results(request, quest_id):
+	q = get_object_or_404(Quest, pk = quest_id)
+	result_list = q.result_set.all()
+	return render_to_response('result.html', {
+				'questionnaire': q,
+				'result_list': result_list
+				})
+
+def answer(request, quest_id, page_id):
+	p = get_object_or_404(Page, order = page_id, quest = quest_id)
+	order = p.order + 1
+	return HttpResponseRedirect(reverse('dlp.chestionare.views.page', args=(quest_id, order)))
--- dlp/settings.py
+++ dlp/settings.py
@@ -1,6 +1,6 @@
 # Django settings for dlp project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
